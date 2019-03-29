7a6fdfccd311ab7cbc1aee66138a9aa4f7060bda
Stefan Devai <stedevai@gmail.com>
2019-03-29 20:22:13 +0100

    Sticky navbar working

--- assets/js/sticky-navbar.js
+++ assets/js/sticky-navbar.js
@@ -1,20 +1,43 @@
-(function() {
-  let scroll = 0;
-  const stickyTrigger = document.getElementById("sticky-menu-trigger").offsetTop;
-  let stickyNav = document.getElementById("sticky-navbar");
-  window.onscroll = () => {
-    if (window.pageYOffset <= 0) {
-      stickyNav.style.opacity = "0";
-    }
-    else {
-      stickyNav.style.opacity = "1";
-    }
+class StickyNavbar {
+  constructor() {
+    let scroll = 0;
+    const stickyTrigger = document.getElementsByClassName("sticky-menu-trigger")[0].offsetTop;
+    let stickyNav = document.getElementById("sticky-navbar");
+    window.onscroll = () => {
+      if (window.pageYOffset <= 0) {
+        stickyNav.style.opacity = "0";
+      }
+      else {
+        stickyNav.style.opacity = "1";
+      }
 
-    if (window.pageYOffset >= stickyTrigger) {
-      stickyNav.style.top = "0";
-    }
-    else {
-      stickyNav.style.top = "-73px";
+      if (window.pageYOffset >= stickyTrigger) {
+        stickyNav.style.top = "0";
+      }
+      else {
+        stickyNav.style.top = "-73px";
+      }
     }
   }
-})();
+
+  smoothScroll(eid) {
+    console.log(eid);
+    const el = document.getElementById(eid);
+    if (eid === 'top') {
+      window.scroll({
+        behavior: 'smooth',
+        left: 0,
+        top: 0
+      });
+    }
+    else {
+      window.scroll({
+        behavior: 'smooth',
+        left: 0,
+        top: el.offsetTop
+      });
+    }
+  }
+}
+  
+let navHome = new StickyNavbar;
