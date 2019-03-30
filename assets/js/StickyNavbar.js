02f961c639456f5b82d1e3506d3f0e772c2ca543
Stefan Devai <stedevai@gmail.com>
2019-03-30 04:48:12 +0100

    Modularized sticky navbar

--- assets/js/StickyNavbar.js
+++ assets/js/StickyNavbar.js
@@ -1,66 +1,89 @@
 class StickyNavbar {
-  constructor(height, ids) {
+  constructor(height) {
     this.height = height;
-    this.ids = ids;
     this.breakpoints = {};
+    this.itemsElements = {};
     this.current_hl_id = '';
 
-    this._highlight();
+    this.stickyTrigger = document.getElementsByClassName("sticky-menu-trigger")[0].offsetTop - this.height;
+    this.stickyNav = document.getElementById("sticky-navbar");
+
     this._manageNavbar();
   }
 
   _manageNavbar() {
-    let scroll = 0;
-    const stickyTrigger = document.getElementsByClassName("sticky-menu-trigger")[0].offsetTop - this.height;
-    let stickyNav = document.getElementById("sticky-navbar");
+    this._prepareHighlight();
 
     window.onscroll = () => {
-      // Nav appears
-      if (window.pageYOffset <= 0) {
-        stickyNav.style.opacity = "0";
-      }
-      else {
-        stickyNav.style.opacity = "1";
-      }
-
-      if (window.pageYOffset >= stickyTrigger) {
-        stickyNav.style.top = "0";
-      }
-      else {
-        stickyNav.style.top = "-" + this.height.toString()  + "px";
-      }
-
-      // Menu highlights
-      let last_key = '';
-      for (let key in this.breakpoints) {
-        if (window.pageYOffset < this.breakpoints[key] && window.pageYOffset > this.breakpoints[last_key] && last_key !== this.current_hl_id) {
-          let last_el = document.getElementById("nav-item-" + this.current_hl_id);
-          if (last_el != null) {
-            last_el.classList.remove("nav-hl");
-          }
-
-          let el = document.getElementById("nav-item-" + last_key);
-          el.classList.add("nav-hl");
-
-          this.current_hl_id = last_key;
-          break;
-        }
-        last_key = key;
-      }
-
+      this._toggleNavbar();
+      this._highlight();
     }
   }
 
-  _highlight() {
-    this.ids.map((id) => {
-      //console.log(id);
-      this.breakpoints[id] = document.getElementById(id).offsetTop - window.innerHeight / 2;
-    });
-    this.breakpoints['end'] = document.body.offsetHeight;
-    //console.log(this.breakpoints);
+  _toggleNavbar() {
+    if (window.pageYOffset <= 0) {
+      this.stickyNav.style.opacity = "0";
+    }
+    else {
+      this.stickyNav.style.opacity = "1";
+    }
+
+    if (window.pageYOffset >= this.stickyTrigger) {
+      this.stickyNav.style.top = "0";
+    }
+    else {
+      this.stickyNav.style.top = "-" + this.height.toString()  + "px";
+    }
   }
 
-  scroll(eid) {
+  _prepareHighlight() {
+    let navItems = document.getElementsByClassName("nav-item");
+    [...navItems].forEach((item) => {
+      let target = item.dataset.target;
+      this.breakpoints[target] = document.getElementById(target).offsetTop - window.innerHeight / 2;
+      this.itemsElements[target] = item;
+
+      item.addEventListener("click", this.scroll, false);
+    });
+    this.breakpoints['end'] = document.body.offsetHeight;
+  }
+
+  _highlight() {
+    let last_key = '';
+    for (let key in this.breakpoints) {
+      if (window.pageYOffset < this.breakpoints[key] && window.pageYOffset > this.breakpoints[last_key] && last_key !== this.current_hl_id) {
+        let last_el = this.itemsElements[this.current_hl_id];
+        if (last_el != null) {
+          last_el.classList.remove("nav-hl");
+        }
+        this.itemsElements[last_key].classList.add("nav-hl");
+        this.current_hl_id = last_key;
+        break;
+      }
+      last_key = key;
+    }
+  }
+
+  scroll() {
+    const eid = this.dataset.target;
+    const el = document.getElementById(eid);
+    if (eid === 'top') {
+      window.scroll({
+        behavior: 'smooth',
+        left: 0,
+        top: 0
+      });
+    }
+    else {
+      window.scroll({
+        behavior: 'smooth',
+        left: 0,
+        top: el.offsetTop
+      });
+    }
+  }
+
+  scrollTo(eid) {
     const el = document.getElementById(eid);
     if (eid === 'top') {
       window.scroll({
