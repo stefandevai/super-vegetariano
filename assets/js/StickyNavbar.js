50dd5e921eeadbf1e23cfd6dbe53d6e2e8f09c77
Stefan Devai <stedevai@gmail.com>
2019-03-30 03:41:55 +0100

    Scroll highlight in navbar

--- assets/js/StickyNavbar.js
+++ assets/js/StickyNavbar.js
@@ -1,6 +1,11 @@
 class StickyNavbar {
-  constructor(height) {
+  constructor(height, ids) {
     this.height = height;
+    this.ids = ids;
+    this.breakpoints = {};
+    this.current_hl_id = '';
+
+    this._highlight();
     this._manageNavbar();
   }
 
@@ -10,6 +15,7 @@ class StickyNavbar {
     let stickyNav = document.getElementById("sticky-navbar");
 
     window.onscroll = () => {
+      // Nav appears
       if (window.pageYOffset <= 0) {
         stickyNav.style.opacity = "0";
       }
@@ -23,8 +29,35 @@ class StickyNavbar {
       else {
         stickyNav.style.top = "-" + this.height.toString()  + "px";
       }
-    }
 
+      // Menu highlights
+      let last_key = '';
+      for (let key in this.breakpoints) {
+        if (window.pageYOffset < this.breakpoints[key] && window.pageYOffset > this.breakpoints[last_key] && last_key !== this.current_hl_id) {
+          let last_el = document.getElementById("nav-item-" + this.current_hl_id);
+          if (last_el != null) {
+            last_el.classList.remove("nav-hl");
+          }
+
+          let el = document.getElementById("nav-item-" + last_key);
+          el.classList.add("nav-hl");
+
+          this.current_hl_id = last_key;
+          break;
+        }
+        last_key = key;
+      }
+
+    }
+  }
+
+  _highlight() {
+    this.ids.map((id) => {
+      //console.log(id);
+      this.breakpoints[id] = document.getElementById(id).offsetTop - window.innerHeight / 2;
+    });
+    this.breakpoints['end'] = document.body.offsetHeight;
+    //console.log(this.breakpoints);
   }
 
   scroll(eid) {
