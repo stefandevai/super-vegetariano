e4322fb2b4650eee69d091029d5a4c720e7ed801
Stefan Devai <stedevai@gmail.com>
2019-04-02 00:59:31 +0200

    Change description in especialidades

--- assets/js/especialidades.js
+++ assets/js/especialidades.js
@@ -1,4 +1,4 @@
-"use stric";
+"use strict";
 
 (function(){
   const grid = document.getElementById("image-mosaic");
@@ -14,17 +14,31 @@
 
         let others = document.getElementsByClassName("foto-especialidad");
         [...others].forEach((other) => {
-          if (!other.classList.contains("primary")) {
+          if (other != this) {
             other.style.opacity = "1";
           }
         });
+
+        // Description
+        const descId = this.dataset.description;
+        let desc = document.getElementById(descId);
+        desc.classList.add("hide-description");
+        desc.style.opacity = "0";
+        let defDesc = document.getElementById("default-description");
+        defDesc.classList.remove("hide-description");
+        defDesc.style.opacity = "1";
       }
 
       else {
         this.classList.add("primary");
         this.style.zIndex = "2";
-        grid.style.gridColumnGap = "0";
-        grid.style.gridRowGap = "0";
+
+        setTimeout((function() {
+          if (this.classList.contains("primary")) {
+            grid.style.gridColumnGap = "0";
+            grid.style.gridRowGap = "0";
+          }
+        }).bind(this), 150);
 
         switch(countNextSiblings(this)) {
           case 3:
@@ -41,13 +55,21 @@
             break;
         }
 
-
         let others = document.getElementsByClassName("foto-especialidad");
         [...others].forEach((other) => {
           if (other != this) {
             other.style.opacity = "0";
           }
         });
+        
+        // Description
+        const descId = this.dataset.description;
+        let desc = document.getElementById(descId);
+        desc.classList.remove("hide-description");
+        desc.style.opacity = "1";
+        let defDesc = document.getElementById("default-description");
+        defDesc.classList.add("hide-description");
+        defDesc.style.opacity = "0";
       }
     }, false);
   });
