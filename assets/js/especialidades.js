fe8104c368da21e346c1ce7843312882976378f4
Stefan Devai <stedevai@gmail.com>
2019-04-02 00:02:15 +0200

    Images scaling in especialidades

--- assets/js/especialidades.js
+++ assets/js/especialidades.js
@@ -1,58 +1,61 @@
 "use stric";
 
-const grid = document.getElementById("image-mosaic");
+(function(){
+  const grid = document.getElementById("image-mosaic");
 
-let imgs = document.getElementsByClassName("foto-especialidad");
-[...imgs].forEach((img) => {
-  img.addEventListener("click", function() { 
-    if (this.classList.contains("primary")) {
-      this.classList.remove("primary");
-      this.style.zIndex = "1";
-      let others = document.getElementsByClassName("foto-especialidad");
-      [...others].forEach((other) => {
-        if (!other.classList.contains("primary")) {
-          other.style.opacity = "1";
-        }
-      });
-    }
+  let imgs = document.getElementsByClassName("foto-especialidad");
+  [...imgs].forEach((img) => {
+    img.addEventListener("click", function() { 
+      if (this.classList.contains("primary")) {
+        this.classList.remove("primary");
+        this.style.zIndex = "1";
+        grid.style.gridColumnGap = "13px";
+        grid.style.gridRowGap = "13px";
 
-    else {
-      this.classList.add("primary");
-      this.style.zIndex = "2";
-
-      switch(countNextSiblings(this)) {
-        case 3:
-          console.log("3 siblings");
-          this.style.transformOrigin = "top left";
-          break;
-        case 2:
-          console.log("2 siblings");
-          this.style.transformOrigin = "top right";
-          break;
-        case 1:
-          console.log("1 siblings");
-          this.style.transformOrigin = "bottom left";
-          break;
-        case 0:
-          console.log("0 siblings");
-          this.style.transformOrigin = "bottom right";
-          break;
+        let others = document.getElementsByClassName("foto-especialidad");
+        [...others].forEach((other) => {
+          if (!other.classList.contains("primary")) {
+            other.style.opacity = "1";
+          }
+        });
       }
 
+      else {
+        this.classList.add("primary");
+        this.style.zIndex = "2";
+        grid.style.gridColumnGap = "0";
+        grid.style.gridRowGap = "0";
 
-      let others = document.getElementsByClassName("foto-especialidad");
-      [...others].forEach((other) => {
-        if (other != this) {
-          other.style.opacity = "0";
+        switch(countNextSiblings(this)) {
+          case 3:
+            this.style.transformOrigin = "top left";
+            break;
+          case 2:
+            this.style.transformOrigin = "top right";
+            break;
+          case 1:
+            this.style.transformOrigin = "bottom left";
+            break;
+          case 0:
+            this.style.transformOrigin = "bottom right";
+            break;
         }
-      });
-    }
-  }, false);
-});
 
-function countNextSiblings(el) {
-  let num_siblings = 0;
-  while (el = el.nextElementSibling)
-    num_siblings++;
-  return num_siblings;
-}
+
+        let others = document.getElementsByClassName("foto-especialidad");
+        [...others].forEach((other) => {
+          if (other != this) {
+            other.style.opacity = "0";
+          }
+        });
+      }
+    }, false);
+  });
+
+  function countNextSiblings(el) {
+    let num_siblings = 0;
+    while (el = el.nextElementSibling)
+      num_siblings++;
+    return num_siblings;
+  }
+})();
