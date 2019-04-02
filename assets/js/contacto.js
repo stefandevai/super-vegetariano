37c10795663bc8d9af1e62f27e4224a256f06ff0
Stefan Devai <stedevai@gmail.com>
2019-04-03 01:23:49 +0200

    Add notification after contact form is sent

---
+++ assets/js/contacto.js
@@ -0,0 +1,45 @@
+(function() {
+  const notificationEl = document.getElementById('notification');
+  const nam = document.getElementById('name-input');
+  const mail = document.getElementById('email-input');
+  const msg = document.getElementById('message-input');
+
+  const url = 'https://clumsy-guineafowl.dev.with-datafire.io/contact';
+  let params = '';
+  let xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      notificationEl.classList.add('success');
+      notificationEl.innerHTML = '¡Gracias! Tu mensaje ha sido enviado.';
+      cleanForm();
+    }
+    else if (xhr.readyState == 4){
+      notificationEl.classList.add('failure');
+      notificationEl.innerHTML = 'Error ' + xhr.status;
+      //xhr = new XMLHttpRequest();
+    }
+  }
+
+  function cleanForm() {
+    nam.value = '';
+    mail.value = '';
+    msg.value = '';
+  }
+
+  function getParams() {
+    return 'name=' + nam.value + '&emailAddress=' + mail.value + '&message=' + msg.value;
+  }
+
+  document.getElementById('submit-button').addEventListener('click', (e) => {
+    if (xhr.readyState == 4) xhr = new XMLHttpRequest();
+    xhr.open('POST', url);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+    e.preventDefault();
+    let params = getParams();
+    console.log(params);
+    xhr.send(params);
+  }, false);
+
+})();
